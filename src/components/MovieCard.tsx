363e4e5e7f8078eee2cd5ebf5735c2c569ce3eae
Felipe <felipe.portillo.delgado@gmail.com>
2022-02-19 14:01:32 -0300

    aplicacao

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,42 @@
+import { Star, Clock } from 'react-feather';
+import { memo } from 'react';
+
+import '../styles/movie-card.scss';
+
+interface MovieCardProps {
+  title: string;
+  poster: string;
+  rating: string;
+  runtime: string;
+}
+
+function MovieCardComponent(props: MovieCardProps) {
+  return (
+    <div className="movie-card">
+      <img
+        src={props.poster}
+        alt={props.title}
+      />
+
+      <div>
+        <div className="movie-info">
+          <span>{props.title}</span>
+          <div className="meta">
+            <div>
+              <Star /> {props.rating}
+            </div>
+
+            <div>
+              <Clock /> {props.runtime}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const MovieCard = memo(MovieCardComponent, (prevProps, nextProps) => {
+  return prevProps.title === nextProps.title && prevProps.poster === nextProps.poster && prevProps.rating === nextProps.rating &&prevProps.runtime === nextProps.runtime;
+  //return Object.is(prevProps, nextProps);
+})
